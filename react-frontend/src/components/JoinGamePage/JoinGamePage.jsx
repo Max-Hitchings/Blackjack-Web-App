901a6d327d21ab69e0fd55b1364a20d43da76994
gk41 <max.hitchings@gmail.com>
2021-03-04 01:24:36 +0000

    Added light and darkmode to join game page

--- react-frontend/src/components/JoinGamePage/JoinGamePage.jsx
+++ react-frontend/src/components/JoinGamePage/JoinGamePage.jsx
@@ -7,33 +7,49 @@ import {
   Input,
   Button,
   Heading,
+  useColorMode,
+  IconButton,
 } from "@chakra-ui/react";
 
+import { BiMoon, BiSun } from "react-icons/bi";
+
 export default function JoinGamePage() {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
-    <Flex
-      width="full"
-      height="80vh"
-      align="center"
-      justifyContent="center"
-      alignSelf="center"
-    >
-      <Box p={2}>
-        <Box textAlign="center">
-          <Heading>Join Game</Heading>
-        </Box>
-        <Box my={4} textAlign="left">
-          <form>
-            <FormControl>
-              <FormLabel>Game Code</FormLabel>
-              <Input type="code" placeholder="*****" maxlength="5" />
-            </FormControl>
-            <Button width="full" mt={4} type="submit">
-              Join Game
-            </Button>
-          </form>
-        </Box>
+    <>
+      <Box textAlign="right" py={4} mr={12}>
+        <IconButton
+          colorScheme={colorMode === "light" ? "blue" : "red"}
+          icon={colorMode === "light" ? <BiMoon /> : <BiSun />}
+          onClick={toggleColorMode}
+          variant="ghost"
+        />
       </Box>
-    </Flex>
+      <Flex
+        width="full"
+        height="80vh"
+        align="center"
+        justifyContent="center"
+        alignSelf="center"
+      >
+        <Box p={2}>
+          <Box textAlign="center">
+            <Heading>Join Game</Heading>
+          </Box>
+          <Box my={4} textAlign="left">
+            <form>
+              <FormControl>
+                <FormLabel>Game Code</FormLabel>
+                <Input type="code" placeholder="*****" maxlength="5" />
+              </FormControl>
+              <Button width="full" mt={4} isActive="true" type="submit">
+                Join Game
+              </Button>
+            </form>
+          </Box>
+        </Box>
+      </Flex>
+    </>
   );
 }
