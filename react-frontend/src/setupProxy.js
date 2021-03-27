0e18d5b24e5816607986aad737e97c4ec02d244b
gk41 <max.hitchings@gmail.com>
2021-03-27 12:55:17 +0000

    changed setupProxy to arrow function

--- react-frontend/src/setupProxy.js
+++ react-frontend/src/setupProxy.js
@@ -1,7 +1,7 @@
 const proxy = require("http-proxy-middleware");
 require("dotenv").config();
 
-module.exports = function (app) {
+module.exports = (app) => {
   app.use(proxy("/api", { target: process.env.DJANGO_PORT }));
   app.use(proxy("/socket.io", { target: process.env.SOCKETIO_PORT }));
 };
