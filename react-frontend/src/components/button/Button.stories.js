ea8e185db889f7d7aefe33a2ecc39033aa3e9836
gk41 <max.hitchings@gmail.com>
2021-04-05 23:11:36 +0100

    Added Button component with stories

---
+++ react-frontend/src/components/button/Button.stories.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Button from "./Button.jsx";
+
+export default {
+  title: "Components/Button",
+  component: Button,
+};
+
+const Template = (args) => <Button test={"shuhs"} />;
+
+export const Primary = Template.bind({});
