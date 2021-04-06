b1585c82118d2ac0734604e3cacf7c1be2629388
gk41 <max.hitchings@gmail.com>
2021-04-06 13:20:03 +0100

    Changes to Button component

--- react-frontend/src/components/button/Button.jsx
+++ react-frontend/src/components/button/Button.jsx
@@ -1,48 +1,43 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
 
-const useStyles = {
+const styles = {
   root: {
     background: (props) =>
-      props.color === "red"
-        ? "linear-gradient(45deg, #FE6B8B 10%, #FF8E53 60%)"
-        : "linear-gradient(45deg, #2196F3 10%, #21CBF3 60%)",
+      props.variant === "Secondary"
+        ? "linear-gradient(-45deg, #27e7ee 10%, #35bdc2 60%)"
+        : "linear-gradient(-45deg, #21f38a 10%, #33bd78 60%)",
     border: 0,
     borderRadius: 3,
     boxShadow: (props) =>
-      props.color === "red"
-        ? "0 3px 5px 2px rgba(255, 105, 135, .3)"
-        : "0 3px 5px 2px rgba(33, 203, 243, .3)",
+      props.variant === "Secondary"
+        ? "0 3px 5px 2px rgba(105, 225, 255, .3)"
+        : "0 3px 5px 2px rgba(75, 199, 71, .3)",
     color: (props) => props.textColor,
     height: 48,
     padding: (props) => `0 ${props.p}px`,
     margin: (props) => `${props.m}px`,
-    "&:hover": {
-      background: (props) =>
-        props.color === "red"
-          ? "linear-gradient(45deg, #FE6B8B 50%, #FF8E53 90%)"
-          : "linear-gradient(45deg, #2196F3 50%, #21CBF3 90%)",
-    },
-    transition: "background 3s ease-out 1000ms",
   },
 };
 
 export function MyBaseComponent(props) {
   const { classes, color, ...other } = props;
-  return <button className={classes.root} {...other} />;
+  return <button className={`${classes.root} styledButton`} {...other} />;
 }
 
-MyBaseComponent.propTypes = {
+Button.propTypes = {
   classes: PropTypes.object.isRequired,
-  color: PropTypes.oneOf(["blue", "red"]).isRequired,
+  variant: PropTypes.oneOf(["Primary", "Secondary"]).isRequired,
+  textColor: PropTypes.string,
 };
 
-MyBaseComponent.defaultProps = {
+Button.defaultProps = {
   textColor: "white",
   p: "30",
   m: "0",
-  hoverColor: "red",
+  textColor: "white",
 };
 
-export const StyledButton = withStyles(useStyles)(MyBaseComponent);
+export const StyledButton = withStyles(styles)(Button);
--- react-frontend/src/components/button/Button.stories.js
+++ react-frontend/src/components/button/Button.stories.js
@@ -5,12 +5,26 @@ import { StyledButton } from "./Button.jsx";
 export default {
   title: "Components/StyledButton",
   component: StyledButton,
+  argTypes: {
+    variant: {
+      control: {
+        type: "radio",
+        options: ["Primary", "Secondary"],
+      },
+    },
+    textColor: {
+      control: {
+        type: "color",
+        defaultValue: "#ffffff",
+      },
+    },
+  },
 };
 
 const Template = (args) => <StyledButton {...args}>TEST</StyledButton>;
 
 export const Primary = Template.bind({});
-Primary.args = { color: "red", p: "30", m: "0" };
+Primary.args = { variant: "Primary" };
 
 export const Secondary = Template.bind({});
-Secondary.args = { color: "blue", p: "30", m: "0" };
+Secondary.args = { variant: "Secondary" };
