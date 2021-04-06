f88cface52512b956701bccac56c9f5d9d00974f
gk41 <max.hitchings@gmail.com>
2021-04-06 17:21:43 +0100

    Updated MainMenu and removed homePage

--- react-frontend/src/components/homePage/MainMenu.jsx
+++ react-frontend/src/components/homePage/MainMenu.jsx
@@ -1,7 +1,50 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import { StyledButton } from "../button/Button.jsx";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: "100vh",
+    width: "100vw",
+    position: "absolute",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  card: {
+    display: "flex",
+    flexDirection: "column",
+    position: "absolute",
+    transform: "translateY(-75%)",
+    backgroundColor: "rgba(58, 58, 58, 0.5)",
+    borderRadius: 15,
+    justifyContent: "center",
+    alignItems: "center",
+    alignContent: "space-between",
+  },
+  header: {
+    fontWeight: 700,
+    color: "#38a169",
+    textShadow: "5px 5px black",
+    fontSize: "45px",
+    margin: "0  50px 0 50px",
+  },
+  buttonWrapper: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "85%",
+    position: "relative",
+    marginTop: "30px",
+    marginBottom: "20px",
+  },
+}));
+
 export default function MainMenu() {
+  const classes = useStyles();
   const history = useHistory();
 
   const handleCreateGame = () => {
@@ -34,30 +77,28 @@ export default function MainMenu() {
   };
 
   return (
-    <div className="MainMenu-Container">
-      <div className="MainMenu-Header">BLACKJACK</div>
-      <div className="MainMenu-ButtonContainer">
-        {/* <Button
-          colorScheme="green"
-          color="black"
-          className="MainMenu-Button"
-          onClick={() => {
-            handleJoinGame();
-          }}
-        >
-          Join Game
-        </Button>
-        <Button
-          colorScheme="green"
-          color="black"
-          className="MainMenu-Button"
-          onClick={() => {
-            handleCreateGame();
-          }}
-        >
-          Create Game
-        </Button> */}
-      </div>
+    <div className={classes.root}>
+      <Card className={classes.card}>
+        <div className={classes.header}>BLACKJACK</div>
+        <div className={classes.buttonWrapper}>
+          <StyledButton
+            textColor="black"
+            onClick={() => {
+              handleJoinGame();
+            }}
+          >
+            JOIN GAME
+          </StyledButton>
+          <StyledButton
+            textColor="black"
+            onClick={() => {
+              handleCreateGame();
+            }}
+          >
+            CREATE GAME
+          </StyledButton>
+        </div>
+      </Card>
     </div>
   );
 }
