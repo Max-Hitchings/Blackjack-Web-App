080fcce29f9915b02ced4319c110c355159be861
gk41 <max.hitchings@gmail.com>
2021-04-11 18:12:10 +0100

    Remade all socket endpoint to work with new DB structure and show everyones hands

--- nodeJS-backend/src/index.js
+++ nodeJS-backend/src/index.js
@@ -1,5 +1,3 @@
-import { leaveGame } from "./util/leaveGame";
-
 require("dotenv").config();
 const express = require("express");
 const fetch = require("node-fetch");
@@ -38,60 +36,85 @@ const apiRouter = require("./routes/api");
 app.use("/api", apiRouter);
 
 io.on("connection", (socket) => {
-  socket.on("join-game", async ({ gameCode }, cb) => {
-    socket.join(gameCode);
-
+  socket.on("pickupCard", async ({ gameCode, playerId }, callBack) => {
     try {
-      await Games.findOne({ gameCode: gameCode }, async (err, result) => {
-        if (result !== null) {
-          console.log(
-            "result",
-            result.players,
-            result.players.includes(socket.id)
-          );
+      // const newGame = await Games.findOneAndUpdate(
+      //   { gameCode: gameCode },
+      //   { $pop: { cards: -1 }, $push: {players: } },
+      //   { useFindAndModify: false }
+      // );
 
-          if (!result.players.includes(socket.id)) {
-            const games = await Games.findOneAndUpdate(
-              { gameCode: gameCode },
-              { $push: { players: socket.id } },
-              { useFindAndModify: false }
-            );
-          }
+      let game = await Games.findOne({ gameCode: gameCode });
+      const newCard = game.cards[0];
+      game.players.forEach((player, index) => {
+        if (player.playerId === playerId) {
+          game.players[index].cards.push({
+            Suit: game.cards[0].Suit,
+            Value: game.cards[0].Value,
+          });
+
+          // game.players[index].cards[game.players[index].cards.length] = {
+          //   suit: "game.cards[0].suit",
+          //   value: "game.cards[0].value",
+          // };
+
+          game.cards.shift();
         }
       });
-    } catch (err) {
-      console.log("error:", err);
-    }
-  });
+      game.save();
+      console.log(game.players);
 
-  socket.on("leave-game", async ({ gameCode, playerId }) => {
-    socket.leave(gameCode);
-    console.log("Removed test = ", gameCode, playerId);
-  });
-
-  socket.on("pickupCard", async ({ gameCode }, callBack) => {
-    try {
-      const newGame = await Games.findOneAndUpdate(
-        { gameCode: gameCode },
-        { $pop: { cards: -1 } },
-        { useFindAndModify: false }
-      );
-      callBack(newGame.cards[0]);
+      io.to(gameCode).emit("updatePlayers", { players: game.players });
+      callBack(newCard);
     } catch (err) {
       console.error(err);
     }
   });
 
+  socket.on("joinGame", async ({ gameCode }) => {
+    socket.join(gameCode);
+    socket.gameCode = gameCode;
+
+    const result = await Games.findOne({ gameCode: gameCode });
+    if (result !== null) {
+      const players = result.players;
+      const hostId = result.hostId;
+      io.to(gameCode).emit("updatePlayers", { players, hostId });
+    }
+  });
+
+  socket.on("startGame", async ({ gameCode }) => {
+    try {
+      await Games.findOneAndUpdate(
+        { gameCode: gameCode },
+        { $set: { started: true } },
+        { useFindAndModify: false }
+      );
+      socket.to(gameCode).emit("gameStarted");
+    } catch (err) {
+      console.error("startGame", err);
+    }
+  });
+
   socket.on("disconnect", async () => {
-    const code = "X26X8";
-    await leaveGame({ Games, code, socket });
-    console.log("user disconnected", socket.id);
+    if (socket.hasOwnProperty("gameCode")) {
+      const result = await Games.findOne({ gameCode: socket.gameCode });
+      if (result !== null) {
+        // const players = [];
+        // result.players.forEach((player) => {
+        //   players.push(player.playerId);
+        // });
+        const players = result.players;
+        const hostId = result.hostId;
+        io.to(socket.gameCode).emit("updatePlayers", { players, hostId });
+      }
+    }
   });
 });
 
-io.of("/").adapter.on("create-room", (room) => {
-  console.log(`room ${room} was created`);
-});
+// io.of("/").adapter.on("create-room", (room) => {
+//   console.log(`room ${room} was created`);
+// });
 
 const LISTEN_PORT = process.env.LISTEN_PORT || 4040;
 
