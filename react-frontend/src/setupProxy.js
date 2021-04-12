c0c642415845ddb833caa18a44e9feefd24b96f6
gk41 <max.hitchings@gmail.com>
2021-04-12 21:59:57 +0100

    Remade setupProxy to try fix bug

--- react-frontend/src/setupProxy.js
+++ react-frontend/src/setupProxy.js
@@ -1,9 +1,27 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 require("dotenv").config();
 
+// module.exports = (app) => {
+//   app.use(createProxyMiddleware("/api", { target: process.env.EXPRESSJS_URL }));
+//   app.use(
+//     createProxyMiddleware("/socket.io", { target: process.env.EXPRESSJS_URL })
+//   );
+// };
+
 module.exports = (app) => {
-  app.use(createProxyMiddleware("/api", { target: process.env.EXPRESSJS_URL }));
   app.use(
-    createProxyMiddleware("/socket.io", { target: process.env.EXPRESSJS_URL })
+    "/api",
+    createProxyMiddleware({
+      target: process.env.EXPRESSJS_URL,
+      changeOrigin: true,
+    })
+  );
+
+  app.use(
+    "/socket.io",
+    createProxyMiddleware({
+      target: process.env.EXPRESSJS_URL,
+      changeOrigin: true,
+    })
   );
 };
