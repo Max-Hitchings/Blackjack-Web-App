9bf5affd416fb5b718f6e4cf48fdc3cf0bc639d2
gk41 <max.hitchings@gmail.com>
2021-10-03 20:38:56 +0100

    minor index.js updates

--- nodeJS-backend/src/index.js
+++ nodeJS-backend/src/index.js
@@ -9,17 +9,17 @@ const http = require("http").createServer(app);
 
 // const DB_URL = process.env.MONGO_URL || process.env.DATABASE_URL;
 const URI = process.env.DATABASE_URL;
-// console.log();
+
 mongoose.connect(URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 const db = mongoose.connection;
 db.on("error", (err) => console.error(err));
-db.once("open", () => console.error("Connected to Database"));
+db.once("open", () => console.log("Connected to Database"));
 
 app.use(express.json());
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
   res.send("<h2>Blackjack WS Server</h2>");
 });
 
@@ -31,5 +31,5 @@ wsController(http);
 
 const LISTEN_PORT = process.env.LISTEN_PORT || 4040;
 http.listen(LISTEN_PORT, () => {
-  console.log(`listening on *:${LISTEN_PORT}`);
+  console.log(`listening on http://127.0.0.1:${LISTEN_PORT}`);
 });
